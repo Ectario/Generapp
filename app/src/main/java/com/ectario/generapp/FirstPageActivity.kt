4a4a4b1da6732b46a290a058cdb9baf99881683b
Ectario <dimitritristangalaxy@gmail.com>
2020-12-17 11:02:13 +0100

    Rename first activity

--- app/src/main/java/com/ectario/generapp/FirstPageActivity.kt
+++ app/src/main/java/com/ectario/generapp/FirstPageActivity.kt
@@ -3,9 +3,9 @@ package com.ectario.generapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FirstPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_firstpage)
     }
 }
