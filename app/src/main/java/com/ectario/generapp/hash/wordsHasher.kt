7171ee8289d121575d9562ba85f3c8b10a6f2eab
Ectario <dimitritristangalaxy@gmail.com>
2021-01-24 20:33:10 +0100

    WordsHasher done and generate password*

--- app/src/main/java/com/ectario/generapp/hash/wordsHasher.kt
+++ app/src/main/java/com/ectario/generapp/hash/wordsHasher.kt
@@ -2,11 +2,13 @@ package com.ectario.generapp.hash
 
 import com.ectario.generapp.HomePageActivity
 import com.ectario.generapp.tools.makeToast
+import kotlin.random.Random
 
-class WordsHasher(listOfWords: ArrayList<String?>? = null, lenghtPasswordArg : Int = 0) : WordsBank(listOfWords, lenghtPasswordArg) {
+class WordsHasher(listOfWords: ArrayList<String?>? = null, lenghtPasswordArg: Int = 0) : WordsBank(listOfWords, lenghtPasswordArg) {
     private val mapingLetter = mapOf('a' to '@', 'e' to '&')
     private var password : String = ""
-    private var noNullPwd = false
+
+
 
     init {
         if (listOfWords != null) {
@@ -17,13 +19,29 @@ class WordsHasher(listOfWords: ArrayList<String?>? = null, lenghtPasswordArg : I
                             mapingLetter.get(it)
                         } else it
                     }
-                    noNullPwd = true
-                } else HomePageActivity.applicationContext.makeToast("Empty word")
+                } else HomePageActivity.applicationContext.makeToast("Empty word found")
             }
-        } else HomePageActivity.applicationContext.makeToast("Empty list of words")
+        } else {
+            password = if (lenghtPasswordArg != 0) generateRandomPassword(lenghtPasswordArg) else generateRandomPassword()
+        }
+    }
+
+    private fun generateRandomPassword(length: Int = Random.Range(6,15)): String {
+        val leftLimit = 33 // char '!' in UTF-8 base 10
+        val rightLimit = 122 // char 'z' in UTF-8 base 10
+        var generatedPwd = ""
+        for (i in 0 until length) {
+            val randomLimitedInt = Random.Range(leftLimit, rightLimit)
+            generatedPwd += randomLimitedInt.toChar()
+        }
+        return generatedPwd
     }
 
     fun getPasswordHashed(): String {
-        return if(noNullPwd) this.password else ""
+        return this.password
+    }
+
+    private fun Random.Range(min : Int, max : Int): Int {
+        return min + (Random.nextFloat() * (max - min + 1)).toInt()
     }
 }
