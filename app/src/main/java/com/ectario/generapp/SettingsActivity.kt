462d2fafe068afc9ab220032b83772cec40e379c
Ectario <dimitritristangalaxy@gmail.com>
2021-03-15 15:00:11 +0100

    First version of theme choose in settings

--- app/src/main/java/com/ectario/generapp/SettingsActivity.kt
+++ app/src/main/java/com/ectario/generapp/SettingsActivity.kt
@@ -2,14 +2,14 @@ package com.ectario.generapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.navigateUp
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import com.ectario.generapp.tools.changingActivity
+import androidx.preference.SwitchPreferenceCompat
+import com.ectario.generapp.tools.ThemeHelper
 
 class SettingsActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: Any
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +28,25 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            setPreferencesFromResource(R.xml.settings_preferences, rootKey)
+            val themePreferences = findPreference<SwitchPreferenceCompat>(getString(R.string.theme_header))
+
+            themePreferences?.let { refreshTheme(it) }
+            
+            themePreferences?.setOnPreferenceClickListener {
+                refreshTheme(it)
+                true
+            }
+        }
+        private fun refreshTheme(pref : Preference){
+            if (!((pref as SwitchPreferenceCompat).isChecked)) {
+                ThemeHelper.applyTheme("dark")
+            }
+            if ((pref as SwitchPreferenceCompat).isChecked) {
+                ThemeHelper.applyTheme("light")
+            }
         }
     }
 }
